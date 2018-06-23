f12647f75f45c186e8adb0135c608788f748d033
Mohit Darmal <mohitdarmal1907@gmail.com>
2018-06-23 13:06:51 +0530

    Add files via upload

--- todo2.js
+++ todo2.js
@@ -9,7 +9,7 @@ var yes = document.querySelector("#yes");
 var no = document.querySelector("#no");
 var reverseALL = document.querySelector("#reverseALL");
 
-var list = []
+
 //When user click on enterButton it start the listData() function 
 enterButton.addEventListener("click", function () {
     listData();
@@ -28,37 +28,48 @@ inputTextBox.addEventListener("keyup", function () {
 
 var listData = function () {
     if (inputTextBox.value !== "") {
-        list.push(inputTextBox.value);
-        var list2 = "";
+        var list = []
+        list.push(inputTextBox.value);        
+        // var list2 = "";
         for (var i = 0; i < list.length; i++) {
-            list2 = list2 + "<p id='arrItem'>" + list[i] + "</p>";
-            todoListDisplay.innerHTML = list2;
+            var para = document.createElement("P");            
+            var paraText = document.createTextNode(inputTextBox.value);
+            para.appendChild(paraText);
+            todoListDisplay.appendChild(para);
+            // list2 = list2 + "<p id='arrItem'>" + list[i] +  "</p>";
+            // todoListDisplay.innerHTML = list;
             inputTextBox.value = null;
             arrList();
+            
+            
         }
     }
-
+    // else if(inputTextBox.value === ""){
+    //     list.pop();
+    // }
 }
 
-
-// var arrList = function () {
-//     var targetP = document.querySelectorAll("#todoListDisplay p");
-//     for (var i = 0; i < targetP.length; i++) {
-//         targetP[i].addEventListener("click", function () {
-//             f1();
-//         });
-//     }
-// }
-
-// when user click on arrya items
 var arrList = function () {
-    var arrItem = document.querySelectorAll("#arrItem");
-    for (var i = 0; i < arrItem.length; i++) {
-        arrItem[i].addEventListener("click", function () {
-            this.classList.toggle("unselect");
+    var targetP = document.querySelectorAll("#todoListDisplay p");
+    for (var i = 0; i < targetP.length; i++) {
+        targetP[i].addEventListener("click", function () {
+            this.classList.add("unselect");              
+            // console.log(this)          
         });
     }
 }
+
+
+
+// when user click on arrya items
+// var arrList = function () {
+//     var arrItem = document.querySelectorAll("#arrItem");
+//     for (var i = 0; i < arrItem.length; i++) {
+//         arrItem[i].addEventListener("click", function () {
+//             this.classList.toggle("unselect");
+//         });
+//     }
+// }
 
 // var f1 = function () {
 //     for(var i = 0; i < arrItem.length; i++){
@@ -78,45 +89,45 @@ var arrList = function () {
 
 //when user click on mark all button 
 markAll.addEventListener("click", function () {
-    var arrItem = document.querySelectorAll("#arrItem");
-    for (var i = 0; i < arrItem.length; i++) {
-        arrItem[i].classList.add("unselect");
+    var targetP = document.querySelectorAll("#todoListDisplay p");
+    for (var i = 0; i < targetP.length; i++) {
+        targetP[i].classList.add("unselect");
     }
 });
 
 //when user click on unmark all button 
 unmarkAll.addEventListener("click", function () {
-    var arrItem = document.querySelectorAll("#arrItem");
-    for (var i = 0; i < arrItem.length; i++) {
-        arrItem[i].classList.remove("unselect");
+    var targetP = document.querySelectorAll("#todoListDisplay p");
+    for (var i = 0; i < targetP.length; i++) {
+        targetP[i].classList.remove("unselect");
     }
 });
 
 //when user click on delete all button 
 deleteAll.addEventListener("click", function () {
-    var arrItem = document.querySelectorAll("#arrItem");
+    var targetP = document.querySelectorAll("#todoListDisplay p");
     var ans = "yes";
     deletePopUp.style.display = "block"
-    for (var i = 0; i < arrItem.length; i++) { 
-        yes.addEventListener("click", function(){
+    for (var i = 0; i < targetP.length; i++) {
+        yes.addEventListener("click", function () {
             list = []
             todoListDisplay.innerHTML = null;
             list2 = ""
             listData();
-            deletePopUp.style.display = "none"            
-        })       
-        no.addEventListener("click", function(){
+            deletePopUp.style.display = "none"
+        })
+        no.addEventListener("click", function () {
             deletePopUp.style.display = "none"
         })
     }
 });
 
-reverseALL.addEventListener("click", function(){
-    var arrItem = document.querySelectorAll("#arrItem");
-    for (var i = 0; i < arrItem.length; i++) {
-        if(markAll){
-            arrItem[i].classList.toggle("unselect");
+reverseALL.addEventListener("click", function () {
+    var targetP = document.querySelectorAll("#todoListDisplay p");
+    for (var i = 0; i < targetP.length; i++) {
+        if (markAll) {
+            targetP[i].classList.toggle("unselect");
         }
-        
+
     }
 });
